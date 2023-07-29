297ee2f655893a189b5aef3e6f3ec0678bbd6829
Dusty Stokes <stokes.dusty@gmail.com>
2023-07-29 00:00:29 -0700

    initial

---
+++ src/resumeData.js
@@ -0,0 +1,127 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Dustin Stokes",
+    "role": "Full Stack Software Developer",
+    "linkedinId":"dustin-stokes-91b4b517",
+    "skypeid": "",
+    "roleDescription": " with 7 years experience looking for the next challenge.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/dustin-stokes-91b4b517/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/stokesdusty",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am a self taught Full Stack Web Developer, with several years of experience writing code for an industry leading webcam streaming platform. I am looking to expand my knowledge, work on some kick ass projects, and write some great code.",
+    "address":"Federal Way, WA",
+    "website":"www.dprimephotography.com www.mtgartistconnection.com",
+    "education":[],
+    "work":[
+      {
+        "CompanyName":"Accretive Technology",
+        "specialization":"Software Developer",
+        "StartingMonth": "June",
+        "StartingYear": "2020",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2023",
+        "Achievements":"Primarily full stack Javascript/Typescript development, implementing a new oAuth system to connect three separate product platforms together, allowing users to signup/login using Google or Twitter. This project allowed us to remove dependency on legacy Db and allow for a much more secure system with JWTs."
+      },
+      {
+        "CompanyName":"Accretive Technology",
+        "specialization":"Asssociate Software Developer",
+        "StartingMonth": "Jan",
+        "StartingYear": "2016",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2020",
+        "Achievements":"As Associate Software Developer, I was on a series of small teams working across ~15-20 repositories using JavaScript (React/Express/Node/Typescript), PHP, Kotlin, Perl, and Java. Notable accomplishment includes modernizing admin and customer service tools from legacy PHP to a more modern React stack with improved data security and component level authorization to protect customer PII."
+      },
+      {
+        "CompanyName":"Motiga",
+        "specialization":"Quality Assurance",
+        "StartingMonth": "June",
+        "StartingYear": "2015",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2016",
+        "Achievements":"Sole test for the web platform for a MOBA style PC game. Served as SCRUM master for web team, and helped organize, write, maintain, and update test suites for multiple teams."
+      }
+    ],
+    "skillsDescription":"Previous working experience with:",
+    "skills":[
+      {
+        "skillname":"TypeScript"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"PHP"
+      },
+      {
+        "skillname":"MySQL"
+      },
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"MongoDb"
+      },
+      {
+        "skillname":"NodeJS"
+      },
+      {
+        "skillname":"Git"
+      },
+      {
+        "skillname":"Docker"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"MtG Artist Connection",
+        "description":"MERN stack website for a hobby of mine. I play Magic the Gathering and have for most of my life. The artists don't get enough attention so I decided to make a tool to display the artists first.",
+        "imgurl":"images/portfolio/mtgartistconnection.jpg",
+        "url": "http://www.mtgartistconnection.com"
+      },
+      {
+        "name":"DPrime Photography",
+        "description":"Simple Wordpress site with custom template to better understand WP implementations and show off some of my photography. Mildly NSFW, as I shoot Burlesque shows.",
+        "imgurl":"images/portfolio/dprimephotography.jpg",
+        "url": "http://www.dprimephotography.com"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Dusty is a supportive, open-minded, and creative team player who clearly articulates his thoughts and helps others develop their own ideas.",
+        "name":"Jeff Thomas"
+      },
+      {
+        "description":"You're a pro at this job.",
+        "name":"Evelyn Stokes, 8 years old"
+      },
+      {
+        "description":"Dustin is a hard working, dedicated individual with a fine eye for detail and a talent for zeroing in on root causes.",
+        "name":"Aidan Spangler"
+      },
+      {
+        "description": "Dusty has a winning team-centered attitude that is infectious and helps everyone on the team around him.",
+        "name": "Justin Woods"
+      },
+      {
+        "description": "I don't know any things to say about code. What should I say?",
+        "name": "Shay Stokes, wife"
+      }
+    ]
+  }
+  
+  export default resumeData
